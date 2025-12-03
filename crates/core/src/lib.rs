13bbc7d8ca68d6fb3175cacfd8650936f6366ee8
Korbin de Man <korbindeman4@gmail.com>
2025-12-03 23:48:31 +0100

    we now re-export all neccesary code in core crate

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -4,4 +4,7 @@ pub mod time;
 pub use session::{PlaybackState, Session};
 pub use time::{MusicalPosition, TimeContext, TimeSignature};
 
-pub use daw_transport::PPQN;
+pub use daw_decode::decode_file;
+pub use daw_project::{Project, ProjectError, save_project};
+pub use daw_render::{render_timeline, ticks_to_samples, write_wav};
+pub use daw_transport::{AudioBuffer, Clip, ClipId, PPQN, Track, TrackId, WaveformData};
